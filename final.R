3c100fdf9205cb3fccc7ad97ba3fef1e7c946e87
AlonBoii <alon.robbins@gmail.com>
2025-03-13 09:14:25 -0700

    plan for the assignment

--- final.R
+++ final.R
@@ -1,2 +1,12 @@
 #heeeeey
 #heeeeeey
+
+# make a function if my fingers are going to be hurt after climbing
+# output: injury or no injury
+# input: how many hours per session, experience level (beginner, intermediate, skilled, pro), hold size (tiny, small, medium, large), hold type (crimp, pinch, jug)
+
+# how many hours per session --> the more hours the more likely to get injured --> more likely to get injured before 30 minutes
+# experience level --> the more skilled the less likely to get injured
+# hold size --> the smaller the hold the more likely to get injured
+# hold type --> crimp is more likely to get injured, then pinch, then jug
+
